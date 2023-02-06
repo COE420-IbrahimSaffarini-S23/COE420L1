6a7d5e508ca05b40f8599cd15be0240a5972e406
Hatecoding-morebusiness <ziadshehab9999@gmail.com>
2023-02-06 18:25:42 +0400

    Feature C added

--- src/Lab1/Lab1Excercise.java
+++ src/Lab1/Lab1Excercise.java
@@ -3,8 +3,8 @@ package Lab1;
 public class Lab1Excercise {
 
 	public static void main(String[] args) {
-		System.out.println("Hellor World I am sending the document from A to B");
-		System.out.println("Hellor World I am recieving the document from A to B");
+		System.out.println("Hellor World I am sending the document from A to c");
+		System.out.println("Hellor World I am recieving the document from A to c");
 	}
 
 }
